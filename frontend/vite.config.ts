faffddf0ff00e24e78570a2cf79c338cbe455342
Ishirai <kirakiseki@163.com>
2023-07-19 22:31:38 +0800

    init: frontend env init

---
+++ frontend/vite.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vite'
+import path from 'node:path'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import { unheadVueComposablesImports } from '@unhead/vue'
+import vue from '@vitejs/plugin-vue'
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
+  plugins: [
+    Pages({
+      extensions: ['vue', 'md'],
+    }),
+
+    Layouts(),
+
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/head',
+        '@vueuse/core',
+        unheadVueComposablesImports,
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/composables',
+        'src/stores',
+      ],
+      vueTemplate: true,
+    }),
+
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/components.d.ts',
+    }),
+
+    VueDevTools(),
+
+    vue(),
+
+
+  ],
+})
