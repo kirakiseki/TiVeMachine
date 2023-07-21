e93d73b5161d6c987f66c22b959f2188ce8883c1
Ishirai <kirakiseki@163.com>
2023-07-21 21:06:15 +0800

    Revert "feat(frontend): add element-plus"
    This reverts commit 4072dc2bf6e7ca27c3cc3daa35eec0f8a65a1392.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,12 +10,6 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
