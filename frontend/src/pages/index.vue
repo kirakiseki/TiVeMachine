d9bf1efe579e17bea55c9cad53d80ff340354846
Ishirai <kirakiseki@163.com>
2023-08-09 23:11:16 +0800

    chore(frontend): delete default index page of vitesse

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -14,34 +14,7 @@ function go() {
 
 <template>
   <div>
-    <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div>
+    Index Page
   </div>
 </template>
 
